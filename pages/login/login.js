980c0da547cde3f5a676aaa82a9b2770f11b1a04
MIKI <1050377194@qq.com>
2022-07-13 16:05:26 +0800

    third test

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,9 +5,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    number:'',
+    code:''
   },
-
+  //获取输入的手机号
+  bindPhone:function(event){
+    // console.log(event.detail.value)
+    this.setData({number:event.detail.value})
+  },
+  //获取输入的验证码
+  bindCode:function(event){
+    // console.log(event.detail.value)
+    this.setData({code:event.detail.value})
+    wx.request({
+      url: 'url',
+    })
+  },
+  login:function(){
+    console.log(this.data.number,this.data.code)
+    wx.request({
+      url:'http://localhost:8000/api/phone_num',
+      data:{phone:this.data.number,code:this.data.code},
+      method:'POST',
+      success:function(res){
+        console.log(res)
+      }
+    })
+  },
+  getCode:function(){
+    var phone_num = this.data.number
+    var arg = /^1(3\d|4[5-9]|5[0-35-9]|6[2567]|7[0-8]|8\d|9[0-35-9])\d{8}$/
+    if(phone_num.length==11&&arg.test(phone_num)){
+      console.log(phone_num)
+      wx.request({
+        url:'http://localhost:8000/api/phone_code',
+        data:{phone:this.data.number},
+        method:'POST',
+        success:function(res){
+          console.log(res)
+        }
+      })
+    }else{
+      wx.showToast({
+        title: '手机号不符',
+        icon:"error"
+      })
+    }
+  },
   /**
    * 生命周期函数--监听页面加载
    */
